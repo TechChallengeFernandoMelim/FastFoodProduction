0896aef928ddbb21ffa4b1d41924a5bf3ace3fe9
Fernando Melim <fernando.lief@hotmail.com>
2024-05-14 23:26:42 -0300

    Implementando teste com BDD

---
+++ FastFoodProduction.Tests/BDD/GetNextOrder.feature
@@ -0,0 +1,10 @@
+﻿Feature: GetNextOrder
+    As a restaurant employee
+    I want to be able to retrieve the next order
+    So that I can process it
+
+Scenario: Successfully retrieve the next order
+    Given there is a valid order available in the messaging service
+    When an employee attempts to retrieve the next order
+    Then the system should return the order details
+    And the order status should indicate it's paid
---
+++ FastFoodProduction.Tests/BDD/GetNextOrderSteps.cs
@@ -0,0 +1,60 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.SQS;
+using FastFoodProduction.Model;
+using FastFoodProduction.Repositories;
+using FastFoodProduction.SqsQueues;
+using FastFoodProduction.UseCases;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TechTalk.SpecFlow;
+
+namespace FastFoodProduction.Tests.BDD;
+
+[Binding]
+public class GetNextOrderSteps
+{
+    private readonly Mock<SqsLogger> loggerMock = new Mock<SqsLogger>(new Mock<AmazonSQSClient>().Object);
+    private readonly Mock<SqsProduction> sqsProductionMock = new Mock<SqsProduction>(new Mock<AmazonSQSClient>().Object);
+    private readonly Mock<ProductionRepository> productionRepositoryMock = new Mock<ProductionRepository>(new Mock<IAmazonDynamoDB>().Object);
+    private GetNextOrderUseCase useCase;
+    private Order retrievedOrder;
+    private IResult result;
+
+    [Given(@"there is a valid order available in the messaging service")]
+    public void GivenThereIsValidOrderAvailableInTheMessagingService()
+    {
+        var order = new Order
+        {
+            InStoreOrderId = "instore",
+            OrderStatus = "Paid",
+            ReceiptHandlerMessage = "receipt"
+        };
+
+        sqsProductionMock.Setup(x => x.GetNextOrder()).ReturnsAsync(order);
+        productionRepositoryMock.Setup(x => x.CreateOrder(order)).ReturnsAsync(true);
+        sqsProductionMock.Setup(x => x.DeleteMessage(It.IsAny<string>())).Returns(Task.CompletedTask);
+    }
+
+    [When(@"an employee attempts to retrieve the next order")]
+    public async Task WhenAnEmployeeAttemptsToRetrieveTheNextOrder()
+    {
+        useCase = new GetNextOrderUseCase();
+        result = await useCase.GetNextOrder(loggerMock.Object, sqsProductionMock.Object, productionRepositoryMock.Object);
+    }
+
+    [Then(@"the system should return the order details")]
+    public void ThenTheSystemShouldReturnTheOrderDetails()
+    {
+        Assert.IsType<Microsoft.AspNetCore.Http.HttpResults.Ok<Order>>(result);
+        var okResult = (Microsoft.AspNetCore.Http.HttpResults.Ok<Order>)result;
+        retrievedOrder = okResult.Value;
+        Assert.NotNull(retrievedOrder);
+    }
+
+    [Then(@"the order status should indicate it's paid")]
+    public void ThenTheOrderStatusShouldIndicateItsPaid()
+    {
+        Assert.Equal("Paid", retrievedOrder.OrderStatus);
+    }
+}
